7543482a732d7b47e4c87b0ade4d634387b83048
Nasibeh Ashtari <n.ashtari@mydigipay.com>
2025-01-05 17:38:00 +0330

    Feature: Add header and meals and mealItem components

---
+++ src/components/MealItem.jsx
@@ -0,0 +1,18 @@
+import { currentFormatter } from '../util/formatting.js';
+import Button from './UI/Button.jsx';
+
+export default function MealItem ( {meal} ) {
+  return <li className="meal-item">
+    <article>
+      <img src={`http://localhost:3000/${meal.image}`} alt={meal.name}/>
+      <div>
+        <h3>{meal.name}</h3>
+        <p className="meal-item-price">{currentFormatter.format(meal.price)}</p>
+        <p className="meal-item-description">{meal.description}</p>
+      </div>
+      <p className="meal-item-actions">
+        <Button>Add to Cart</Button>
+      </p>
+    </article>
+  </li>;
+}
